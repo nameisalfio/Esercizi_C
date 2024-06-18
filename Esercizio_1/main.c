98ae48c4917089380081c8be814ca35bb85c634b
nameisalfio <alfio.spoto@outlook.it>
2024-06-18 11:47:01 +0200

    Improvedxercise_1

--- Esercizio_1/main.c
+++ Esercizio_1/main.c
@@ -21,33 +21,49 @@ Numero di vocali: 976
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
+
+// return true if 'c' is vowels
+bool isVowel(char c) {
+    c = tolower(c);
+    if (c =='a' || c == 'e' || c == 'i' || c == 'o' || c== 'u') 
+        return true;
+    return false;
+}
 
 int main(int argc, char *argv[]){
 
-    //faccio il controllo di errore sul numero di argomenti
+    //faccio il controllo di errore sul numero di argomenti (il primo è il nome dell'eseguibile)
     if (argc != 2){
         printf("Error: %s", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
     //tento di aprire il file in modalità read
-    FILE * file = fopen(argv[1], "r");
-    if (file == NULL){
+    FILE * fp = fopen(argv[1], "r");
+    if (fp == NULL){
         perror("Errore nell'apertura del file");
+        exit(EXIT_FAILURE);
     }
 
     int count = 0;
-    int c; // memorizza il carattere letto dal file di testo
-    while ((c =fgetc(file)) != EOF){ //fget legge il carattere successivo puntato da file che è il puntatore a file fino alla fine del fine (EOF)
-        c = tolower(c); //se il carattere è maiuscolo tolower lo converte in minuscolo (libreria ctype.h)
-        if (c =='a' || c == 'e' || c == 'i' || c == 'o' || c== 'u') {
-            count++;
-        }
+    char c; // memorizza il carattere letto dal file di testo
+    while ((c = fgetc(fp)) != EOF){ //fgetc legge il carattere successivo puntato da file che è il puntatore a file fino alla fine del fine (EOF)
+        if(isVowel(c)) count++;
     }
 
-    fclose(file); //chiudo il file dopo che l'ha letto tutto
-    printf ("Il numero di vocali é:  %d\n", count);
+    /*
+    //variante con fgets
+    char buffer[100];
+    while(fgets(buffer, strlen(buffer), fp)){
+        for (int i = 0; i < strlen(buffer); i++)
+            if(isVowel(c)) count++;
+    }
+    */
 
-
+    fclose(fp); //chiudo il file dopo che l'ha letto tutto
+    printf ("Il numero di vocali é :  %d\n", count);
+    exit(EXIT_SUCCESS);
 }
 
 
